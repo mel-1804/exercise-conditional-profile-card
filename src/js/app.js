a9f2518cdc6dda034708e2ce343444682857cb43
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-07-25 15:35:28 +0000

    termine el proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -33,26 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name ? variables.name + " " : "Name"} ${
-    variables.lastName ? variables.lastName + " " : "Lastname"
+          <h1>${variables.name ? variables.name : "Name"} ${
+    variables.lastName ? variables.lastName : "Lastname"
   }</h1>
-          <h2>${variables.role ? variables.role + " " : "Role"}</h2>
-          <h3>${variables.city ? variables.city + " " : "City"} ${
-    variables.country ? variables.country + " " : "Country"
+          <h2>${variables.role ? variables.role : "Role"}</h2>
+          <h3>${variables.city ? variables.city : "City"} ${
+    variables.country ? variables.country : "Country"
   }</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href=${
+            <li><a href="https://twitter.com/${
               variables.twitter
-            }><i class="fab fa-twitter"></i></a></li>
-            <li><a href=${
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
               variables.github
-            }><i class="fab fa-github"></i></a></li>
-            <li><a href=${
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
               variables.linkedin
-            }><i class="fab fa-linkedin"></i></a></li>
-            <li><a href=${
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
               variables.instagram
-            }><i class="fab fa-instagram"></i></a></li>
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
